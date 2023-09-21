d1284eceb1aefb2820fec105b6cda33d7676bc97
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-09-21 15:42:47 +0300

    add new task from UI

--- introduceAngular/src/app/app.component.ts
+++ introduceAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoItem } from './models/todoItem.model';
+import { fakeItems } from './models/todoItem.mock';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,7 @@ export class AppComponent {
 
   countries:string[] = ['Türkiye','Canada','Netherlands','France'];
 
-  todoItems: TodoItem[] =
-  [
-    new TodoItem('Çiçekleri Sula',false),
-    new TodoItem('Angular Öğren',false),
-    new TodoItem('ASP.NET Core ile API Yaz',true)  
-  ];
+  todoItems: TodoItem[] = fakeItems
 
 
 
@@ -34,11 +30,7 @@ export class AppComponent {
       this.todoItems = this.todoItems.filter(t=>!t.isDone);
     }
     else{
-      this.todoItems = [
-        new TodoItem('Çiçekleri Sula',false),
-        new TodoItem('Angular Öğren',false),
-        new TodoItem('ASP.NET Core ile API Yaz',true)  
-      ];
+      this.todoItems =fakeItems;
     }
 
   }
---
+++ introduceAngular/src/app/models/todoItem.mock.ts
@@ -0,0 +1,7 @@
+import { TodoItem } from "./todoItem.model";
+
+export const fakeItems: TodoItem[] = [
+    new TodoItem('Çiçekleri Sula', false),
+    new TodoItem('Angular Öğren', false),
+    new TodoItem('ASP.NET Core ile API Yaz', true)
+];
